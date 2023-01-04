881748c656ab517e8382e122430f90e4d4d46baa
omer evron <omerevron011@gmail.com>
2023-01-04 17:11:57 +0200

    reorganized Pages

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,26 @@
+
+import React from "react";
+import { Hero, Navbar, Footer,ShowCase } from "../Comps";
+import styles from "../styles";
+
+const Landing = () => {
+  return (
+    <div className="background w-full overflow-hidden">
+
+    <div className={`${styles.boxWidth} mx-auto`}>
+      <Hero></Hero>
+    </div>
+<div className="h-screen flex justify-center items-center">
+      <ShowCase />
+</div>
+
+    <div className={`bg-primary ${styles.flexStart} `}>
+      <div className={`${styles.boxWidth}`}>
+        <Footer></Footer>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Landing
