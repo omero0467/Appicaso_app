2b4a479ca17a4dc449a2390e9275c5452c057788
omer evron <omerevron011@gmail.com>
2023-01-04 22:39:01 +0200

    image interface

--- src/Comps/Output.jsx
+++ src/Comps/Output.jsx
@@ -3,7 +3,7 @@ import SkeletonLoader from "./SkeletonLoader";
 
 const Output = ({outputRespone}) => {
   return (
-    <div className="glass  md:ml-8 md:w-full h-[800px]">
+    <div className="glass md:ml-8 md:w-3/6 h-[800px]">
       <h1 className="text-center text-6xl font-poppins font-bold my-8 text-white">
         Output
       </h1>
--- src/Comps/Preview.jsx
+++ src/Comps/Preview.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const Preview = ({src, className,onLoad}) => {
   return (
-    <div className={`max-w-[512px] ${className}`}>
+    <div className={`max-w-[800px] ml-auto ${className}`}>
+        <h1 className='text-center text-6xl font-poppins font-bold my-8 text-white'>Your Image</h1>
         {src&&<img onLoad={onLoad} className='object-scale-down max-h-[600px]' src={src} alt="your image" />}
     </div>
   )
